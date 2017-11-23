2d8427c1fa67bc385d3f1ec418b7e4c4a37c7722
patrickswijgman <petrik09@live.nl>
2017-11-23 17:19:49 +0100

    session info first draft

--- front-end/src/components/Car.js
+++ front-end/src/components/Car.js
@@ -24,38 +24,59 @@ class Car extends Component {
         if (data.sector_1_time && data.sector_2_time)
             sectorThreeTime = data.current_lap_time - data.sector_1_time - data.sector_2_time;
 
-        // Gap
-        let gap = "";
+        // Interval
+        let interval = "";
         if (data.car_ahead) {
-            gap = data.car_ahead.current_lap_time - data.current_lap_time;
-            gap = gap.toFixed(3);
-            gap = "+" + gap;
+            interval = data.car_ahead.current_lap_time - data.current_lap_time;
+            interval = interval.toFixed(3);
+            interval = "+" + interval;
         }
 
         // Convert time in seconds to a readable format
         let bestLapTime = Helper.secondsToStr(data.best_lap_time);
         let currentLapTime = Helper.secondsToStr(data.current_lap_time);
+        let lastLapTime = Helper.secondsToStr(data.last_lap_time);
         sectorOneTime = Helper.secondsToStr(sectorOneTime);
         sectorTwoTime = Helper.secondsToStr(sectorTwoTime);
         sectorThreeTime = Helper.secondsToStr(sectorThreeTime);
 
+        //Check if current lap is invalid
+        if (data.current_lap_invalid) {
+            currentLapTime = (<b className="red">INVALID</b>);
+        }
+
         // Check if in pits
-        let inPits = "";
-        if (data.in_pits === 1) inPits = "Pitting";
-        if (data.in_pits === 2) inPits = "In Pit Area";
+        let pits = 0;
+        if (data.in_pits === 1 || data.in_pits === 2)
+            pits = (<b className="red">IN PIT</b>);
+
+        // Check car position
+        let carPosition = data.car_position;
+        if (carPosition <= 3) {
+            let trophyColor = "";
+            if (carPosition === 1) trophyColor = "gold";
+            if (carPosition === 2) trophyColor = "silver";
+            if (carPosition === 3) trophyColor = "bronze";
+            carPosition = (<i className={"fa fa-trophy " + trophyColor} aria-hidden="true" />);
+        }
+
+        // Fetch driver name
+        let driver = "jipjop13";
 
         // Initial state
         this.state = {
-            car_position: data.car_position,
+            car_position: carPosition,
             driver_id: data.driver_id,
-            gap: gap,
+            driver: driver,
+            interval: interval,
             best_lap_time: bestLapTime,
+            last_lap_time: lastLapTime,
             current_lap_time: currentLapTime,
             sector_1_time: sectorOneTime,
             sector_2_time: sectorTwoTime,
             sector_3_time: sectorThreeTime,
             current_lap_number: data.current_lap_number,
-            in_pits: inPits,
+            in_pits: pits,
             tyre_compound: data.tyre_compound,
             car_ahead: data.car_ahead,
             car_behind: data.car_behind,
@@ -63,17 +84,23 @@ class Car extends Component {
 
     }
 
+    handleOnClick(event) {
+        event.preventDefault();
+        alert(this.state.driver_id);
+    }
+
     render() {
         return (
-            <tr className="Car">
+            <tr className="Car" onClick={(e) => this.handleOnClick(e)}>
                 <td>{this.state.car_position}</td>
-                <td>{this.state.driver_id}</td>
-                <td>{this.state.gap}</td>
+                <td>{this.state.driver}</td>
+                <td>{this.state.interval}</td>
                 <td>{this.state.best_lap_time}</td>
                 <td>{this.state.current_lap_time}</td>
                 <td>{this.state.sector_1_time}</td>
                 <td>{this.state.sector_2_time}</td>
                 <td>{this.state.sector_3_time}</td>
+                <td>{this.state.last_lap_time}</td>
                 <td>{this.state.current_lap_number}</td>
                 <td>{this.state.in_pits}</td>
                 <td><Tire type={this.state.tyre_compound} /></td>
---
+++ front-end/src/components/Session.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import Helper from "../utilities/Helper";
+import '../stylesheets/Session.css';
+
+const sessionTypes = ["Unknown", "Practice", "Qualifying", "Race"];
+const trackNames = [
+    "Melbourne",
+    "Sepang",
+    "Shanghai",
+    "Sakhir (Bahrain)",
+    "Catalunya",
+    "Monaco",
+    "Montreal",
+    "Silverstone",
+    "Hockenheim",
+    "Hungaroring",
+    "Spa",
+    "Monza",
+    "Singapore",
+    "Suzuka",
+    "Abu Dhabi",
+    "Texas",
+    "Brazil",
+    "Austria",
+    "Sochi",
+    "Mexico",
+    "Baku (Azerbaijan)",
+    "Sakhir Short",
+    "Silverstone Short",
+    "Texas Short",
+    "Suzuka Short",
+];
+const flagColors = ["none", "green", "none", "yellow", "red"];
+const flagLabels = ["", "All clear", "", "Danger", "Session stopped"];
+
+class Session extends Component {
+
+    render() {
+
+        let session = this.props.data.session;
+
+        // Show laps when it is a race, else show time left
+        let showLapsOrTimeLeft = null;
+        if (session.session_type === 3) {
+            showLapsOrTimeLeft = (
+                <div className="session-block">
+                    <b>Total laps</b>
+                    <div>{session.total_laps}</div>
+                </div>
+            )
+        } else {
+            let timeLeft = Helper.secondsToStr(session.session_time_left, false);
+            showLapsOrTimeLeft = (
+                <div className="session-block">
+                    <b>Session time left</b>
+                    <div>{timeLeft}</div>
+                </div>
+            );
+        }
+
+        // Get session type
+        let sessionType = sessionTypes[session.session_type];
+
+        // Get map name
+        let trackName = trackNames[session.track_number];
+
+        // Get flag
+        let flagColor = flagColors[session.vehicle_fia_flags];
+        let flagLabel = flagLabels[session.vehicle_fia_flags];
+
+        return (
+            <div className="Session">
+                <div className="row">
+                    <div className="col-6">
+                        <div className="session-block">
+                            <b>Track name</b>
+                            <div>{trackName}</div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="session-block">
+                            <b>Session type</b>
+                            <div>{sessionType}</div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        {showLapsOrTimeLeft}
+                    </div>
+                    <div className="col-6">
+                        <div className={"session-block bg-" + flagColor}>
+                            <b>Flag</b>
+                            <div>{flagLabel}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Session;
