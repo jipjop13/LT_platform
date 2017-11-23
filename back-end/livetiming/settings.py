03e898dd29ebe10547ff6a916a8d088c11d5a127
Rick Venema <r_venema13@hotmail.com>
2017-11-23 20:30:28 +0100

    fixing dockers

--- back-end/livetiming/settings.py
+++ back-end/livetiming/settings.py
@@ -75,11 +75,30 @@ WSGI_APPLICATION = 'livetiming.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+#
+# DATABASES = {
+# 	'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'livetiming',
+#         'USER': 'root',
+#         'PASSWORD': 'somepassword',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'livetiming',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': 'SET storage_engine=INNODB,default_storage_engine=INNODB,character_set_connection=utf8mb4,collation_connection=utf8mb4_unicode_ci'
+        }
     }
 }
 
