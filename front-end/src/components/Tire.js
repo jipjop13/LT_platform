c85e4df2e2a2e03de3b3d060136a1cfbf4f0a081
patrickswijgman <petrik09@live.nl>
2017-11-28 22:10:02 +0100

    Improved tire component

--- front-end/src/components/Tire.js
+++ front-end/src/components/Tire.js
@@ -8,22 +8,46 @@ const tireTypesLabels = ["Ultra soft", "Super soft", "Soft", "Medium", "Hard", "
 
 class Tire extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: null,
+            label: null
+        }
+    }
+
     componentDidMount() {
         $(function () {
             $('[data-toggle="tooltip"]').tooltip()
         });
     }
 
+    componentWillMount() {
+        this.updateState(this.props);
+    }
+
+    componentWillReceiveProps(props) {
+        this.updateState(props);
+    }
+
+    updateState(props) {
+        let color = tireTypes[props.type];
+        let label = tireTypesLabels[props.type];
+
+        this.setState({
+            color: color,
+            label: label
+        })
+    }
+
     render() {
-        let type = this.props.type;
-        let color = tireTypes[type];
-        let label = tireTypesLabels[type];
         return (
             <div className="Tire"
                  data-toggle="tooltip"
                  data-placement="left"
-                 title={label} >
-                <Icon icon="circle-o-notch" color={color} />
+                 title={this.state.label} >
+                <Icon icon="circle-o-notch" color={this.state.color} />
             </div>
         );
     }
